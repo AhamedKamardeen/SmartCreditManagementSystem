0fed98b86502a978593c447ffe433c4c142ada6a
Ahamedkamardeen <ahamedkamardeen.s@gmail.com>
2025-11-02 20:15:35 +0530

    Comiited Smart management system And create services

---
+++ credit-service/credit-service/src/test/java/com/scms/credit_service/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scms.credit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

07cdff2009230c9758dc8c2c9380653fbf1b3d2a
kamardeen.s <kamardeen.s@sysarc-lt-575.sysarcin.com>
2026-01-03 10:45:37 +0530

    add five dummy user

--- user-service/user-service/src/main/java/com/scms/user_service/config/DataInitializer.java
+++ user-service/user-service/src/main/java/com/scms/user_service/config/DataInitializer.java
@@ -2,6 +2,9 @@ package com.scms.user_service.config;
 import com.scms.user_service.entity.User;
 import com.scms.user_service.repo.UserRepo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,13 +20,32 @@ public class DataInitializer {
         return args -> {
 
             if (userRepository.count() == 0) {
+            	
+            	List<User> users = new ArrayList<>();
 
-                User user = new User();
-                user.setUsername("admin");
-                user.setPassword(passwordEncoder.encode("admin123"));
-                user.setRole("USER");
-                System.err.println("User Saved................");
-                userRepository.save(user);
+            	User admin = new User();
+            	admin.setUsername("admin");
+            	admin.setPassword(passwordEncoder.encode("admin123"));
+            	admin.setRole("ADMIN");
+
+            	users.add(admin);
+            	System.err.println("Admin Added................");
+            	for (int i = 1; i <= 14; i++) {
+            	    User user = new User();
+            	    user.setUsername("user" + i);
+            	    user.setPassword(passwordEncoder.encode("user" + i));
+            	    user.setRole("USER");
+
+            	    users.add(user);
+            	    System.err.println("User"+i+" Added................");
+            	}
+
+//                User user = new User();
+//                user.setUsername("admin");
+//                user.setPassword(passwordEncoder.encode("admin123"));
+//                user.setRole("USER");
+                System.err.println("All User Saved................");
+                userRepository.saveAll(users);
             }
         };
     }
